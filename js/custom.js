0231fdb1c2496668c480fcea6d67290e4d40e309
ysj0211 <siyul7139@gmail.com>
2024-07-12 15:52:33 +0900

    portfolio_0712

--- js/custom.js
+++ js/custom.js
@@ -2,7 +2,7 @@
 	// 변수 ht에 브라우저의 높이 값을 지정
 	let ht = $(window).height();
 	$('section').height(ht);
-
+	
 	// 1. 브라우저가 리사이즈될 때마다 브라우저와 section의 높이값을 갱신
 	$(window).on('resize', function() {
 		let ht = $(window).height();
@@ -46,7 +46,6 @@
 
 	// 4. 마우스 휠
 	$('section').on("mousewheel", function(event, delta) {
-
 		if (delta > 0) {
 			// 마우스 휠을 올렸을 때 실행할 구문
 			let prev = $(this).prev().offset().top;
@@ -60,6 +59,58 @@
 				scrollTop : next
 			}, 1400);
 		}
-
 	});
+
+	// 5. 프로젝트 버튼
+	const popWidth = 1000;
+  const popHeight = 750;
+
+	const project1 = document.getElementById("project1");
+	project1.addEventListener('click', () => {
+		let left = (screen.availWidth - popWidth) / 2 ;
+		let top = (screen.availHeight - popHeight) / 2 ;
+		window.open('./notice1.html', 'notice', `width=${popWidth} height=${popHeight} left=${left} top=${top}`);
+	})
+
+	const project2 = document.getElementById("project2");
+	project2.addEventListener('click', () => {
+		let left = (screen.availWidth - popWidth) / 2 ;
+		let top = (screen.availHeight - popHeight) / 2 ;
+		window.open('./notice2.html', 'notice', `width=${popWidth} height=${popHeight} left=${left} top=${top}`);
+	})
+
+	const project3 = document.getElementById("project3");
+	project3.addEventListener('click', () => {
+		let left = (screen.availWidth - popWidth) / 2 ;
+		let top = (screen.availHeight - popHeight) / 2 ;
+		window.open('./notice3.html', 'notice', `width=${popWidth} height=${popHeight} left=${left} top=${top}`);
+	})
+
+	const project4 = document.getElementById("project4");
+	project4.addEventListener('click', () => {
+		let left = (screen.availWidth - popWidth) / 2 ;
+		let top = (screen.availHeight - popHeight) / 2 ;
+		window.open('./notice4.html', 'notice', `width=${popWidth} height=${popHeight} left=${left} top=${top}`);
+	})
+
+	const project5 = document.getElementById("project5");
+	project5.addEventListener('click', () => {
+		let left = (screen.availWidth - popWidth) / 2 ;
+		let top = (screen.availHeight - popHeight) / 2 ;
+		window.open('./notice5.html', 'notice', `width=${popWidth} height=${popHeight} left=${left} top=${top}`);
+	})
+
+	const project6 = document.getElementById("project6");
+	project6.addEventListener('click', () => {
+		let left = (screen.availWidth - popWidth) / 2 ;
+		let top = (screen.availHeight - popHeight) / 2 ;
+		window.open('./notice6.html', 'notice', `width=${popWidth} height=${popHeight} left=${left} top=${top}`);
+	})
+
+	const project7 = document.getElementById("project7");
+	project7.addEventListener('click', () => {
+		let left = (screen.availWidth - popWidth) / 2 ;
+		let top = (screen.availHeight - popHeight) / 2 ;
+		window.open('./notice7.html', 'notice', `width=${popWidth} height=${popHeight} left=${left} top=${top}`);
+	})
 });
